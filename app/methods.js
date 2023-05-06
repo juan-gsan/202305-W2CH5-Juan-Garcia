e54efab21c40c6bc7c87ffdd05143abd7bec21a7
Silvia <silvia.garbas@gmail.com>
2023-05-06 17:16:45 +0200

    Added findMethod and someMethod functions and tests

--- app/methods.js
+++ app/methods.js
@@ -16,8 +16,10 @@ export const pushMethod = (elements) => {
   return length + 1;
 };
 
+console.log(pushMethod([1, 2, 3, 4, 5]));
+
 export const findMethod = (elements, testedElement) => {
-  testedElement = 5;
+  // TestedElement = 5;
   for (let element = 0; element < elements.length; element++) {
     if (elements[element] === testedElement) {
       return true;
@@ -26,3 +28,30 @@ export const findMethod = (elements, testedElement) => {
 
   return false;
 };
+
+console.log(findMethod([1, 2, 3], 6));
+
+export const someMethod = (elements, parameter) => {
+  const newLength = lengthMethod(elements);
+
+  for (let i = 0; i < newLength; i++) {
+    if (elements[i] === parameter) {
+      return true;
+    }
+  }
+};
+
+console.log(someMethod([1, 2, 3], 2));
+
+export const popMethod = (elements) => {
+  if (elements.length === 0) {
+    return undefined;
+  }
+
+  const lastElement = elements[elements.length - 1];
+  elements = elements.length - 1;
+  // Array.length -= 1;
+  return lastElement;
+};
+
+console.log(popMethod([1, 2, 3]));
--- app/methods.test.js
+++ app/methods.test.js
@@ -1,98 +1,103 @@
-import { lengthMethod, pushMethod, findMethod, mapMethod } from "./methods";
+import { lengthMethod, pushMethod, findMethod, someMethod, popMethod} from "./methods";
 
-describe("Given a lenghtMehod function", () => {
-  describe("When it receives [1, 2, 3 ,4]", () => {
-    test("Then it should return 4", () => {
-      const testedElements = [1, 2, 3, 4];
-      const expectedOutput = 4;
+// describe("Given a lenghtMehod function", () => {
+//   describe("When it receives [1, 2, 3 ,4]", () => {
+//     test("Then it should return 4", () => {
+//       const testedElements = [1, 2, 3, 4];
+//       const expectedOutput = 4;
 
-      const length = lengthMethod(testedElements);
+//       const length = lengthMethod(testedElements);
 
-      expect(length).toBe(expectedOutput);
-    });
-  });
+//       expect(length).toBe(expectedOutput);
+//     });
+//   });
 
-  describe("When it receives []", () => {
-    test("Then it should return 0", () => {
-      const testedElements = [];
-      const expectedOutput = 0;
+//   describe("When it receives []", () => {
+//     test("Then it should return 0", () => {
+//       const testedElements = [];
+//       const expectedOutput = 0;
 
-      const length = lengthMethod(testedElements);
+//       const length = lengthMethod(testedElements);
 
-      expect(length).toBe(expectedOutput);
-    });
-  });
-});
+//       expect(length).toBe(expectedOutput);
+//     });
+//   });
+// });
 
-describe("Given a pushMethod function", () => {
-  describe("When it receives 'casa'", () => {
-    test("Then it should return 3", () => {
-      const testedElements = ["perro", "gato"];
-      const expectedOutput = 3;
+// describe("Given a pushMethod function", () => {
+//   describe("When it receives 'casa'", () => {
+//     test("Then it should return 3", () => {
+//       const testedElements = ["perro", "gato"];
+      
+//       const expectedOutput = 3;
 
-      const output = pushMethod(testedElements);
+//       const output = pushMethod(testedElements);
+
+//       expect(output).toBe(expectedOutput);
+//     });
+//   });
+
+//   describe("When it receives 5", () => {
+//     test("Then it should return 6", () => {
+//       const testedElements = [1, 2, 3, 4, 5];
+//       const expectedOutput = 6;
+
+//       const output = pushMethod(testedElements);
+
+//       expect(output).toBe(expectedOutput);
+//     });
+//   });
+// });
+
+// describe("Given a findMethod function", () => {
+//   describe("When it receives [1, 2, 3, 4, 5];, and the testedElement '8'", () => {
+//     test("Then it should return true", () => {
+//       const testedElements = [1, 2, 3, 4, 5];
+//       const testedElement = 8;
+//       const expectedOutput = false;
+
+//       const output = findMethod(testedElements);
+
+//       expect(output).toBe(expectedOutput);
+//     });
+//   });
+
+//   describe("When it receives ['sol', 'arena', 'mar'] and 'sol'", () => {
+//     test("Then it should return true", () => {
+//       const testedElements = ['sol', 'arena', 'mar'];
+//       const testedElement = "sol";
+//       const expectedOutput = true;
+
+//       const output = findMethod(testedElements, testedElement);
+
+//       expect(output).toBe(expectedOutput);
+//     });
+//   });
+// });
+
+describe("Given a someMethod function", () => {
+  describe("When it receives [1,2,3,4,5] and NaN", () => {
+    test("Then it should return true", () => {
+      const testedElements = [ 1, 2, 3, 4, 5];
+      const testedElement = 5;
+      const expectedOutput = true;
+
+      const output = someMethod(testedElements, testedElement);
 
       expect(output).toBe(expectedOutput);
     });
   });
 
-  describe("When it receives 5", () => {
-    test("Then it should return 6", () => {
-      const testedElements = [1, 2, 3, 4, 5];
-      const expectedOutput = 5;
-
-      const output = pushMethod(testedElements);
-
-      expect(output).toContain(expectedOutput);
-    });
-  });
-});
-
-describe("Given a findMethod function", () => {
-  describe("When it receives [1,2,3,4,5]", () => {
-    test("Then it should return true", () => {
-      const testedElements = [1, 2, 3, 4, 5];
-      const expectedOutput = true;
-
-      const output = findMethod(testedElements);
-
-      expect(output).toStrictEqual(expectedOutput);
-    });
-  });
-
-  describe("When it receives ['casa', 'gato', 'perro']", () => {
-    test("Then it should return true", () => {
-      const testedElements = ["casa", "gato", "perro"];
-      const testedElement = "libro";
+  describe("When it receives ['casa', 'gato', 'perro' ] and 'vaso'", () => {
+    test("Then it should return false", () => {
+      const testedElements = ['casa', 'gato','perro']
+      const testedElement = 'vaso';
       const expectedOutput = false;
 
-      const output = findMethod(testedElements, testedElement);
+      const output = someMethod(testedElements, testedElement);
 
-      expect(output).toStrictEqual(expectedOutput);
+      expect(output).toBe(expectedOutput);
     });
   });
 });
 
-// Describe("Given a mapMethod function", () => {
-//   describe("When it receives ['casa', 'patata', 'gato']", () => {
-//     test("Then it should return ['casa', 'patata', 'gato']", () => {
-//       const testedElements = ["casa", "patata", "gato"];
-//       const expectedOutput = ["casa", "patata", "gato"];
-
-//       const output = mapMethod(testedElements);
-
-//       expect(output).toStrictEqual(expectedOutput);
-//     });
-//   });
-
-//   describe("When it receives [1,2,3,4,5]", () => {
-//     test("Then it should return [1,2,3,4,5]", () => {
-//       const testedElements = [1, 2, 3, 4, 5];
-//       const expectedOutput = [1, 2, 3, 4, 5];
-
-//       const output = mapMethod(testedElements);
-
-//       expect(output).toStrictEqual(expectedOutput);
-//     });
-//   });
-// });
